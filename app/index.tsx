64815faea9ac7d88b9f7784178255cbf23a1fe5b
Rodrigo <reisrodrigo2003@gmail.com>
2025-03-05 23:15:34 +0100

    fix(auth): redirect unauthenticated users to login page

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,20 @@
-import { Redirect } from 'expo-router';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Redirect, router } from 'expo-router';
+import { useEffect } from 'react';
 
 export default function Index() {
+  useEffect(() => {
+    const fetchProfile = async () => {
+        const storedUser = await AsyncStorage.getItem("user");
+        if (!storedUser) {
+          const token = await AsyncStorage.getItem("token");
+          if (!token) {
+            router.replace("/auth/login");
+            return;
+          }
+        }
+    };
+    fetchProfile();
+  }, []);
   return <Redirect href="/tabs" />;
 }
